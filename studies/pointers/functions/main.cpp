43e482b92cb3ab82d3491a32404a2c869b1fad88
thiagobergami <thiagobergamig@gmail.com>
2023-08-15 18:53:50 -0300

    Function Pointers

---
+++ studies/pointers/functions/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+
+// This code will add a function pointer to another function.
+void ForEach(const std::vector<int> &values, void(*func)(int))
+{
+    for (int value : values)
+        func(value);
+}
+
+int main()
+{
+    std::vector<int> values = {1, 5, 4, 2, 3};
+    //
+    ForEach(values, [](int value) { std::cout << "Value: " << value << std::endl; });
+
+    std::cin.get();
+}
